aa7262fe1e9b51967a338f34af243723e61cd989
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-31 14:02:26 +0530

    Search 63

--- BinarySearchTrees/BinarySearchTrees/Program.cs
+++ BinarySearchTrees/BinarySearchTrees/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinarySearchTrees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Binary<int> bst = new Binary<int>(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(11);
+            bst.Insert(65);
+            bst.Insert(3);
+            bst.Insert(16);
+            bst.Insert(63);
+            bst.Insert(67);
+            Console.WriteLine("Binary Search Tree ");
+            bst.Display();
+            bst.Size();
+            bool find = bst.search(63, bst);
+            Console.ReadLine();
+        }
+    }
+}
